0d3b08c59c111428af5b6af0f5d6e03e48accef4
raikausami <36069675+raikausami@users.noreply.github.com>
2018-04-05 23:24:18 +0900

    Add files via upload

--- b.py
+++ b.py
@@ -3,9 +3,15 @@ import urllib.request
 import bs4
 import os
 import time
-url = "https://search.yahoo.co.jp/image/search?ei=UTF-8&fr=sfp_as&aq=-1&oq=&ts=2406&p=%E3%83%94%E3%82%AB%E3%83%81%E3%83%A5%E3%82%A6&meta=vc="
+#url = "https://search.yahoo.co.jp/image/search?ei=UTF-8&fr=sfp_as&aq=-1&oq=&ts=2406&p=%E3%83%94%E3%82%AB%E3%83%81%E3%83%A5%E3%82%A6&meta=vc="
 #url="https://search.yahoo.co.jp/image/search?p=ピカチュウ&search.x=1&tid=top_ga1_sa&ei=UTF-8&aq=-1&oq=ピカチュウ&ai=ZxjGa2pET3CuwckLL5TU.A&ts=1955&fr=top_ga1_sa"
 #url="https://search.yahoo.co.jp/image/search;_ylt=A2RioudeIsJa_VsATiaU3uV7?p=yahoo&aq=-1&oq=&ei=UTF-8"
+import sys
+argvs=sys.argv
+url=argvs[1]
+#url="https://search.yahoo.co.jp/image/search?ei=UTF-8&fr=sfp_as&aq=-1&oq=&ts=5293&p=%E9%95%B7%E6%BF%B1%E3%81%AD%E3%82%8B&meta=vc="
+#print(urlt)
+#print(ult)
 request = urllib.request.urlopen(url)
 html = request.read()
 
